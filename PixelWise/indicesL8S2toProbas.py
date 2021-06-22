0312c5ed1b4efe669b1a9d30764d6c17bccbc0a6
Dmitry Kolesov <kolesov.dm@gmail.com>
2021-06-22 15:45:31 +0300

    Add regularization

--- PixelWise/indicesL8S2toProbas.py
+++ PixelWise/indicesL8S2toProbas.py
@@ -19,7 +19,7 @@ TRAIN_FILE_PATH = os.path.join(FOLDER, TRAINING_BASE+'.tfrecord.gz')
 TEST_FILE_PATH = os.path.join(FOLDER, EVAL_BASE+'.tfrecord.gz')
 
 MODEL_DIR = 'models'
-MODEL_NAME = 'model_v0_0_6'
+MODEL_NAME = 'model_v0_0_7'
 MODEL_PATH = os.path.join(MODEL_DIR, MODEL_NAME)
 
 
@@ -59,20 +59,25 @@ train_dataset = train_dataset.map(parse_tfrecord, num_parallel_calls=5)
 def to_tuple(inputs, targets):
     return (tf.expand_dims(tf.transpose(list(inputs.values())), 1), tf.expand_dims(targets, 1))
 
-# Map the to_tuple function, shuffle and batch.
+# TRAIN_SIZE = 725884
+# EVAL_SIZE = 364127
 BATCH_SIZE = 1024 * 32
-input_dataset = train_dataset.map(to_tuple).shuffle(BATCH_SIZE).batch(BATCH_SIZE)
-test_dataset = tf.data.TFRecordDataset(TEST_FILE_PATH, compression_type='GZIP').map(parse_tfrecord, num_parallel_calls=5).map(to_tuple).batch(1024*16)
+BUFFER_SIZE = BATCH_SIZE
+
+# Map the to_tuple function, shuffle and batch.
+input_dataset = train_dataset.map(to_tuple).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)  # .repeat()
+test_dataset = tf.data.TFRecordDataset(TEST_FILE_PATH, compression_type='GZIP').map(parse_tfrecord, num_parallel_calls=5).map(to_tuple).batch(BATCH_SIZE)  # .repeat()
 
 
 # Define the layers in the model.
+regularization_w = 0.0001
 model = tf.keras.models.Sequential([
     tf.keras.layers.Input((None, None, len(BANDS),)),
-    tf.keras.layers.Conv2D(64, (1,1), activation=tf.nn.relu),
+    tf.keras.layers.Conv2D(64, (1,1), activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.L2(regularization_w)),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv2D(64, (1,1), activation=tf.nn.relu),
+    tf.keras.layers.Conv2D(64, (1,1), activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.L2(regularization_w)),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv2D(1, (1,1), activation='sigmoid')
+    tf.keras.layers.Conv2D(1, (1,1), activation='sigmoid', kernel_regularizer=tf.keras.regularizers.L2(regularization_w))
 ])
 
 # Compile the model with the specified loss function.
@@ -80,9 +85,6 @@ model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.metrics.binary
               
 
 # Fit the model to the training data.
-model.fit(x=input_dataset, validation_data=test_dataset, epochs=250)
-model.fit(x=input_dataset, validation_data=test_dataset, epochs=250)
-model.fit(x=input_dataset, validation_data=test_dataset, epochs=250)
 model.fit(x=input_dataset, validation_data=test_dataset, epochs=250)
 
 model.save(MODEL_PATH, save_format='tf')
